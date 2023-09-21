2370293ed958d8f28283f1ce56e0e36d8a70df7c
Julia Pineda <Julia.Pineda@analog.com>
2023-09-21 14:00:45 +0800

    Add option to get packages file or default
    Signed-off-by: Julia Pineda <Julia.Pineda@analog.com>

--- test/test_packages.py
+++ test/test_packages.py
@@ -1,7 +1,7 @@
 import pytest
 import utils
 
-@pytest.mark.parametrize("name", utils.get_packages())
+@pytest.mark.parametrize("name", utils.get_packages('default'))
 def test_packages(host, name):
     pkg = host.package(name)
     assert pkg.is_installed
