0ba299392a0f35d4aa8ecec3cf60e6725f1c48b3
Trecia Agoylo <trecia.agoylo@analog.com>
2025-04-07 14:37:37 +0800

    add skip for kuiper related tests
    Signed-off-by: Trecia Agoylo <trecia.agoylo@analog.com>

--- test/test_services.py
+++ test/test_services.py
@@ -1,6 +1,10 @@
 import pytest
 import utils
 
+@pytest.mark.skipif(
+    not pytest.config.getoption("-m") == "kuiper",
+    reason="Skipping because the 'kuiper' marker is not passed"
+)
 @pytest.mark.kuiper
 @pytest.mark.parametrize("name", utils.get_services())
 def test_services(host, name):
